88df6b705e1cf41c2e1a1dce328a32c000b84bd9
lolilukia <lolilukia@163.com>
2017-04-14 23:08:58 +0800

    nav and slider

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import './assets/libs/jquery-3.2.1.min'
+import './assets/libs/bootstrap/css/bootstrap.min.css'
+import './assets/libs/bootstrap/js/bootstrap.min'
+import 'element-ui/lib/theme-default/index.css'
+import './assets/libs/pushy-buttons.min.css'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
-})
+});
